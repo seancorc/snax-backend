bbae097165f664b2ae7cc06b9ab64229ab3b3e72
Kyle Infantino <kyleinfantino@dhcp-clara-5064.redrover.cornell.edu>
2019-04-24 23:01:25 -0400

    git

--- db.py
+++ db.py
@@ -2,6 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+association_table = db.Table('association', db.Model.metadata,
+    db.Column('cart_id', db.Integer, db.ForeignKey('cart.id')),
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id')))
+
 class Restaurant(db.Model):
     __tablename__ = 'restraunt'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,6 +40,38 @@ class Menu(db.Model):
         'id': self.id,
         'name': self.name,
         'restaurantID': self.restaurantID
+        }
+
+
+
+class Food(db.Model):
+    __tablename__ = 'food'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    cart = db.relationship("Cart", secondary = association_table, back_populates = "food")
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+
+    def serialize(self):
+        return{
+        'id': self.id,
+        'name': self.name
+        }
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    food = db.relationship("Food", secondary = association_table, back_populates = "cart")
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+
+    def serialize(self):
+        return{
+        'id': self.id,
+        'name': self.name
         }
 
 class User(db.Model):
