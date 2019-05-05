fba3398383dc5fb9d1e0b703161d393bcaa1d94a
Sean Corcoran <seancorcoran45@icloud.com>
2019-05-05 02:10:13 -0400

    Gates G01 Grind

--- app.py
+++ app.py
@@ -14,40 +14,44 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-#Test.
+# Test.
 @app.route('/api/snax/')
 def snax():
     return json.dumps("snax")
 
-#Restaurants
+# Restaurants
 @app.route('/api/snax/restaurants/')
 def getRestaurants():
     restaurants = Restaurant.query.all()
-    get = {'success': True, 'data': [restaurant.serialize() for restaurant in restaurants]}
+    get = {'success': True, 'data': [
+        restaurant.serialize() for restaurant in restaurants]}
     return json.dumps(get), 200
+
 
 @app.route('/api/snax/restaurant/<restaurant_name>/')
 def getSpecificRestaurant(restaurant_name):
-    restaurant = Restaurant.query.filter_by(name = restaurant_name).first()
+    restaurant = Restaurant.query.filter_by(name=restaurant_name).first()
     if restaurant is not None:
         get = {'success': True, 'data': restaurant.serialize()}
         return json.dumps(get), 200
     else:
         return json.dumps({'success': False, 'error': 'Restaurant not found!'}), 404
 
-@app.route('/api/snax/restaurants/', methods = ['POST'])
-def create_restaurant():
-        rest_body = json.loads(request.data)
-        restaurant = Restaurant(
-            name = rest_body.get('name'),
-            )
-        db.session.add(restaurant)
-        db.session.commit()
-        return json.dumps({'success': True, 'data': restaurant.serialize()}), 201
 
-@app.route('/api/snax/restaurant/<restaurant_name>/', methods = ['Delete'])
+@app.route('/api/snax/restaurant/', methods=['POST'])
+def create_restaurant():
+    rest_body = json.loads(request.data)
+    restaurant = Restaurant(
+        name=rest_body.get('name'),
+    )
+    db.session.add(restaurant)
+    db.session.commit()
+    return json.dumps({'success': True, 'data': restaurant.serialize()}), 201
+
+
+@app.route('/api/snax/restaurant/<restaurant_name>/', methods=['Delete'])
 def delete_restaurant(restaurant_name):
-    restaurant = Restaurant.query.filter_by(name = restaurant_name).first()
+    restaurant = Restaurant.query.filter_by(name=restaurant_name).first()
     if restaurant is not None:
         db.session.delete(restaurant)
         db.session.commit()
@@ -55,27 +59,29 @@ def delete_restaurant(restaurant_name):
     return json.dumps({'success': False, 'error': 'Task not found!'}), 404
 
 
-#Food
+# Food
 @app.route('/api/snax/restaurant/<restaurant_name>/food/')
 def getFood(restaurant_name):
-    restaurant = Restaurant.query.filter_by(name = restaurant_name).first()
+    restaurant = Restaurant.query.filter_by(name=restaurant_name).first()
     if restaurant is not None:
-        get = {'success': True, 'data': [food.serialize() for food in restaurant.food]}
+        get = {'success': True, 'data': [
+            food.serialize() for food in restaurant.food]}
         return json.dumps(get), 200
     else:
         return json.dumps({'success': False, 'error': 'Restaurant not found!'}), 404
 
-@app.route('/api/snax/restaurant/<restaurant_name>/food/', methods = ['POST'])
+
+@app.route('/api/snax/restaurant/<restaurant_name>/food/', methods=['POST'])
 def create_food(restaurant_name):
-    restaurant = Restaurant.query.filter_by(name = restaurant_name).first()
+    restaurant = Restaurant.query.filter_by(name=restaurant_name).first()
     if restaurant is not None:
         body = json.loads(request.data)
         food = Food(
-            name = body.get('name'),
-            restaurant_id = restaurant.id,
-            price = body.get('price'),
-            description = body.get('description'),
-            comments = body.get('comments')
+            name=body.get('name'),
+            restaurant_id=restaurant.id,
+            price=body.get('price'),
+            description=body.get('description'),
+            comments=body.get('comments')
         )
         restaurant.food.append(food)
         db.session.add(food)
@@ -84,72 +90,78 @@ def create_food(restaurant_name):
     return json.dumps({'success': False, 'error': 'Restaurant not found!'}), 404
 
 
-#Users
+# Users
 @app.route('/api/snax/users/')
 def getUsers():
     users = User.query.all()
     get = {'success': True, 'data': [user.serialize() for user in users]}
     return json.dumps(get), 200
 
+
 @app.route('/api/snax/user/<int:user_id>/')
 def getSpecificUser(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
         get = {'success': True, 'data': user.serialize()}
         return json.dumps(get), 200
     else:
         return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
-@app.route('/api/snax/users/', methods = ['POST'])
+
+@app.route('/api/snax/users/', methods=['POST'])
 def create_user():
     user_body = json.loads(request.data)
     user = User(
-        name = user_body.get('name')
-        )
+        name=user_body.get('name')
+
+    )
     db.session.add(user)
     db.session.commit()
     return json.dumps({'success': True, 'data': user.serialize()}), 201
 
-@app.route('/api/snax/users/<int:user_id>/', methods = ['Delete'])
+
+@app.route('/api/snax/users/<int:user_id>/', methods=['Delete'])
 def delete_user(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
         db.session.delete(user)
         db.session.commit()
         return json.dumps({'success': True, 'data': user.serialize()}), 201
     return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
-#Orders
+# Orders
 @app.route('/api/snax/orders/')
 def getOrders():
     orders = Order.query.all()
     get = {'success': True, 'data': [order.serialize() for order in orders]}
     return json.dumps(get), 200
 
-@app.route('/api/snax/orders/<int:order_id>/', methods = ['Delete'])
+
+@app.route('/api/snax/orders/<int:order_id>/', methods=['Delete'])
 def delete_order(order_id):
-    order = Order.query.filter_by(id = order_id).first()
+    order = Order.query.filter_by(id=order_id).first()
     if order is not None:
         db.session.delete(order)
         db.session.commit()
         return json.dumps({'success': True, 'data': order.serialize()}), 201
     return json.dumps({'success': False, 'error': 'Order not found!'}), 404
 
-#User Actions
-@app.route('/api/snax/order/<int:user_id>/', methods = ['POST'])
+# User Actions
+@app.route('/api/snax/order/<int:user_id>/', methods=['POST'])
 def create_order(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
-        neworder = Order(ordered_id = user_id)
+        neworder = Order(userID=user_id)
         user.orders = user.orders + [neworder]
         db.session.add(neworder)
         db.session.commit()
-        return json.dumps({'success': True, 'data': neworder.serialize()}),200
+        return json.dumps({'success': True, 'data': neworder.serialize()}), 200
     return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
-@app.route('/api/snax/placeorder/<int:user_id>/', methods = ['POST'])
+# Need updating
+@app.route('/api/snax/placeorder/<int:user_id>/', methods=['POST'])
 def place_order(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
         if user.order is not None:
             user.order.active = True
@@ -158,11 +170,12 @@ def place_order(user_id):
         return json.dumps({'success': False, 'error': 'Order not found!'}), 404
     return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
-@app.route('/api/snax/fulfillorder/<int:order_id>/<int:user_id>/', methods = ['POST'])
+# Need updating
+@app.route('/api/snax/fulfillorder/<int:order_id>/<int:user_id>/', methods=['POST'])
 def fulfillorder(order_id, user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
-        order = Order.query.filter_by(id = order_id).first()
+        order = Order.query.filter_by(id=order_id).first()
         if order is not None:
             order.deliverUser = user
             order.matched = True
@@ -171,15 +184,37 @@ def fulfillorder(order_id, user_id):
         return json.dumps({'success': False, 'error': 'Order not found!'}), 404
     return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
+
 @app.route('/api/snax/orders/<int:user_id>/')
 def getUserOrders(user_id):
-    user = User.query.filter_by(id = user_id).first()
+    user = User.query.filter_by(id=user_id).first()
     if user is not None:
-        orders = Order.query.filter_by(orderedid = user_id)
+        orders = Order.query.filter_by(userID=user_id)
         serialized = [order.serialize() for order in orders]
         return json.dumps({'success': True, 'data': serialized}), 201
     return json.dumps({'success': False, 'error': 'User not found!'}), 404
 
+
+@app.route('/api/snax/orders/<int:order_id>/', methods=["POST"])
+def addFoodToOrder(order_id):
+    order = Order.query.filter_by(id=order_id).first()
+    post_body = json.loads(request.data)
+    # userID = post_body.get('userID')
+    restaurantName = post_body.get('restaurantName')
+    FoodName = post_body.get('foodName')
+    print(FoodName)
+    restaurant = Restaurant.query.filter_by(name=restaurantName).first()
+    if restaurant is None:
+        return json.dumps({'success': False, 'error': 'Restaurant not found!'}), 404
+    food = Food.query.filter_by(
+        name=FoodName, restaurant_id=restaurant.id).first()
+    if order is None or food is None:
+        print(order)
+        print(food)
+        return json.dumps({'success': False, 'error': 'Order not found!'}), 404
+    order.food = order.food + [food]
+    serialized = order.serialize()
+    return json.dumps({'success': True, 'data': serialized}), 201
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -2,117 +2,154 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-#Many to One Food to Restaurant
-#One to One Order to User
-#Many to One Food to Order
+# Many to One Food to Restaurant
+# One to One Order to User
+# Many to One Food to Order
 
-association_table = db.Table('association', db.Model.metadata,
-    db.Column('order_id', db.Integer, db.ForeignKey('order.id')),
-    db.Column('food_id', db.Integer, db.ForeignKey('food.id')))
+association_table = db.Table('association1', db.Model.metadata,
+                             db.Column('order_id', db.Integer,
+                                       db.ForeignKey('order.id')),
+                             db.Column('food_id', db.Integer, db.ForeignKey('food.id')))
 
-userorder_tabel = db.Table('association', db.Model.metadata,
-    db.Column('order_id', db.Integer, db.ForeignKey('order.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')))
+userorder_tabel = db.Table('association2', db.Model.metadata,
+                           db.Column('order_id', db.Integer,
+                                     db.ForeignKey('order.id')),
+                           db.Column('user_id', db.Integer, db.ForeignKey('user.id')))
+
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True)
-    food = db.relationship('Food', cascade = 'delete')
+    food = db.relationship('Food', cascade='delete')
     name = db.Column(db.String, nullable=False)
-
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
         self.food = []
 
-
     def serialize(self):
         return {
-        'id': self.id,
-        'name': self.name,
-        'menu': [food.serialize() for item in self.food]
+            'id': self.id,
+            'name': self.name,
+            'menu': [item.serialize() for item in self.food]
         }
+
 
 class Food(db.Model):
     __tablename__ = 'food'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable = False)
-    orders = db.relationship("Order", secondary= association_table, back_populates = 'food')
-    price = db.Column(db.Float, nullable = False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(
+        'restaurant.id'), nullable=False)
+    orders = db.relationship(
+        "Order", secondary=association_table, back_populates='food')
+    price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String)
     comments = db.Column(db.String)
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
-        self.restaurant_id =kwargs.get('restaurant_id')
+        self.restaurant_id = kwargs.get('restaurant_id')
         self.price = kwargs.get('price')
-        self.description = kwargs.get('description','')
-        self.comments = kwargs.get('comments','')
+        self.description = kwargs.get('description', '')
+        self.comments = kwargs.get('comments', '')
         self.orders = []
 
     def serialize(self):
         return{
-        'id': self.id,
-        'name': self.name,
-        'price': self.price,
-        'description': self.description,
-        'comments': self.comments
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'comments': self.comments
         }
+
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String, nullable = False)
-    orders = db.relationship("Order", secondary= association_table, back_populates = 'user')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    orderer = db.Column(db.Boolean, nullable=False)
+    deliverer = db.Column(db.Boolean, nullable=False)
+    orders = db.relationship(
+        "Order")
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
         self.orders = []
+        self.orderer = True
+        self.deliverer = False
 
     def serialize(self):
-        if self.order is not None:
+        if self.orders is not None:
             return{
-            'id': self.id,
-            'name': self.name,
-            'orders': self.orders.length()
+                'id': self.id,
+                'name': self.name,
+                'orders': [order.subSerialize() for order in self.orders]
             }
         else:
             return{
-            'id': self.id,
-            'name': self.name,
-            'orders': 0
+                'id': self.id,
+                'name': self.name,
+                'orders': []
             }
+
+    def subSerialize(self):
+        if self.orders is not None:
+            return{
+                'id': self.id,
+                'name': self.name,
+            }
+
 
 class Order(db.Model):
     __tablename__ = 'order'
-    id = db.Column(db.Integer, primary_key = True)
-    orderedid = db.relationship("User", secondary= association_table, back_populates = 'order')
-    deliverid = db.relationship("User", secondary= association_table, back_populates = 'order')
-    matched = db.Column(db.Boolean, nullable = False)
-    active = db.Column(db.Boolean, nullable = False)
-    food = db.relationship("Food", secondary= association_table, back_populates = 'order')
-
+    id = db.Column(db.Integer, primary_key=True)
+    matched = db.Column(db.Boolean, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    food = db.relationship(
+        "Food", secondary=association_table, back_populates='orders')
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, **kwargs):
         self.matched = False
-        self.orderedid = kwargs.get('ordered_id')
-        self.deliverid = None
         self.active = False
         self.food = []
+        self.userID = kwargs.get('userID')
 
     def serialize(self):
-        if self.deliverUser is None:
-            delivering = None
+        print(self.userID)
+        print('fsafdsaf')
+        for f in self.food:
+            if f is not None:
+                print(f.serialize())
+
+        user = User.query.filter_by(id=self.userID).first()
+        user = user.subSerialize()
+        otherOrderList = Order.query.filter_by(id=self.id)
+        otherOrder = None
+        for order in otherOrderList:
+            if order != self:
+                otherOrder = order
+        if otherOrder is None:
+            otherUser = None
         else:
-            deliveruser = User.query.filter_by(id = self.deliverid)
-            delivering = deliverser.serialize()
-        ordereduser = User.query.filter_by(id = self.orderedid)
+            otherUser = otherOrder.user
+            otherUser = otherUser.serialize()
         return{
-        'id': self.id,
-        'matched': self.matched,
-        'orderer': ordereduser.serialize(),
-        'placed': self.active,
-        'food': [food.serialize() for food in self.food],
-        'deliverer': delivering
+            'id': self.id,
+            'matched': self.matched,
+            'user': user,
+            'placed': self.active,
+            'food': [food.serialize() for food in self.food],
+            'otherUser': otherUser
+        }
+
+    def subSerialize(self):
+
+        return{
+            'id': self.id,
+            'matched': self.matched,
+            'placed': self.active,
+            'food': [food.serialize() for food in self.food],
         }
